5b5ea43f14c2843f795c49fb2e0479f82a8f865f
sneshev <steffal.n2@gmail.com>
2025-05-26 22:27:27 +0200

    test

--- src/main.c
+++ src/main.c
@@ -2,7 +2,7 @@
 
 int main()
 {
-    for (int i = 0; i < 9999; i++)
+    for (int i = 0; i < 999; i++)
         printf("i am in loove with the mitani branch <33 (becuase I love MITANI!!!)\n");
     return (0);
 }
