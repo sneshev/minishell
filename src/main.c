9ac28a4e1916a10e553e163286a884c9c86c06fd
Mitani Misumi <mmisumi@f1r4s9.codam.nl>
2025-05-27 16:41:13 +0200

    yo

--- src/main.c
+++
@@ -1,8 +0,0 @@
-#include <stdio.h>
-
-int main()
-{
-    for (int i = 0; i < 9999; i++)
-        printf("i am in loove with the mitani branch <33 (becuase I love MITANI!!!)\n");
-    return (0);
-}
--- src/minishell.h
+++ src/minishell.h
@@ -1,8 +1,11 @@
-#ifndef MINISHELL
-# define MINISHELL
+#ifndef MINISHELL_H
+# define MINISHELL_H
 
-
-
+# include "../libft/libft.h"
+# include <stdio.h>
+# include <readline/readline.h>
+# include <readline/history.h>
+# include <stdlib.h>
 
 
 
