57e4b60951feb9c64b7ce7f181bc8e2abf298459
sneshev <steffal.n2@gmail.com>
2025-06-01 18:37:54 +0200

    structure

--- src/main.c
+++ src/main.c
@@ -12,18 +12,14 @@
 
 #include "minishell.h"
 
-void	minishell(char *line)
+void execute_line()
 {
-	(void)line;
+	
 }
 
-int main(int argc, char *argv[], char *envp[])
+void	minishell(char *envp[])
 {
-	(void)argv;
-	if (argc != 1)
-		return (1);
-	enable_signals();
-	while(1)
+	while (1)
 	{
 		char *line = readline("minishell$ ");
 		if (!line || ft_strncmp(line, "exit", 5) == 0)
@@ -34,9 +30,18 @@ int main(int argc, char *argv[], char *envp[])
 		if (g_signal == SIGINT)
 			receive_SIGINT();
 		if (is_valid_input(line, envp) == true)
-			minishell(line);
+			execute_line(line);
 		else
 			handle_invalid_input();
 	}
+}
+
+int main(int argc, char *argv[], char *envp[])
+{
+	(void)argv;
+	if (argc != 1)
+		return (1);
+	enable_signals();
+	minishell(envp);
 	return (0);
 }
