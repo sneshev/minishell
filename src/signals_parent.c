67cee0d15adccd3d2b5ef2a6577a843f1a4bca91
John Doe <steffal.n2@gmail.com>
2025-06-10 16:04:42 +0200

    restructure

--- src/signals_parent.c
+++ src/signals_parent.c
@@ -9,6 +9,16 @@
 
 volatile sig_atomic_t	g_signal = 0;
 
+// Ctrl + c (only outside of process)
+void receive_SIGINT()
+{
+    g_signal = 0;
+    ft_putstr_fd("\n", STDERR_FILENO);
+    rl_replace_line("", 0);
+	rl_on_new_line();
+	rl_redisplay();
+}
+
 void redirect_SIGINT()
 {
     struct sigaction	sa;
