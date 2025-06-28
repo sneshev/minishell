a2a1d96ba4c6d576eb68d94a3c41f0f71dc55bf1
Mitani Misumi <winnitytrinnity@Mitanis-MacBook-Air.local>
2025-06-29 00:03:57 +0200

    its working but still so messy

--- src/execute.c
+++ src/execute.c
@@ -26,7 +26,7 @@ int count_pids(t_list *list)
     return (count);
 }
 
-int	setup_input(t_list *list, int *pip, int *prev_pipe)
+void	setup_input(t_list *list, int *prev_pipe)
 {
 	if (list->input == -2)
 	{
@@ -35,18 +35,29 @@ int	setup_input(t_list *list, int *pip, int *prev_pipe)
 			dup2(*prev_pipe, STDIN_FILENO);
 			close (*prev_pipe);
 		}
-		if (list->next)
-		{
-			dup2(pip[WRITE], STDOUT_FILENO);
-			close(pip[WRITE]);
-		}
 	}
 	if (list->input >= 0)
 	{
 		dup2(list->input, STDIN_FILENO);
 		close (list->input);
 	}
+}
 
+void	setup_output(t_list *list, int *pip)
+{
+	if (list->output == -2)
+	{
+		if (list->next)
+		{
+			dup2(pip[WRITE], STDOUT_FILENO);
+			close(pip[WRITE]);
+		}
+	}
+	if (list->output >= 0)
+	{
+		dup2(list->output, STDOUT_FILENO);
+		close(list->output);
+	}
 }
 
 void	child_process(t_list *list, int *pip, int *prev_pipe, char **envp)
@@ -60,140 +71,14 @@ void	child_process(t_list *list, int *pip, int *prev_pipe, char **envp)
 			close(pip[WRITE]);
 			close(pip[READ]);
 		}
-		exit ;
+		exit(1) ;
 	}
-	// SETUP_INPUT
-	if (setup_input == -1)
-		return ;
-	if (list->input >= 0)
-	{
-		dup2(list->input, STDIN_FILENO);
-		close (list->input);
-		close(pip[READ]);
-		// close prev_pipe if it exists
-		if (*prev_pipe != -1)
-			close (*prev_pipe);
-	}
-	// if we have no infile as stdin
-	else if (list->input < 0)
-	{
-		// if we have a prev pipe
-		if (*prev_pipe != -1)
-		{
-			dup2(*prev_pipe, STDIN_FILENO);
-			close(*prev_pipe);
-		}
-		// if we have no infile or pipe input, we simply close
-		close(pip[READ]);
-	}
-	// SETUP OUTPUT
-	if (list->output >= 0)
-	{
-		dup2(list->output, STDOUT_FILENO);
-		close (list->output);
-		close(pip[WRITE]);
-	}
-	// if theres no outfile
-	else if (list->output < 0)
-	{
-		// if there is a next node, meaning there is a pipe
-		if (list->next)
-			dup2(pip[WRITE], STDOUT_FILENO);
-		// no outfile and pipe output, we simply close and write to terminal
-		close(pip[WRITE]);
-	}
+	setup_input(list, prev_pipe);
+	setup_output(list, pip);
+
 	execve(list->cmd, list->args, envp);
 	error_message("execve_fail", 127);
 }
-
-// check for invalid files -> we will then not enter the child process and go right to the next list
-// if files are valid, and theres a list->next, we pipe and enter child process (prev_pipe is still -1 so its doesnt exist yet)
-// we check if theres an infile/outfile by checking its value, if this is -2 we dont set it up as it doesnt exist
-// if theres an infile we dup2 this to stdin and close prev_pipe if this is not -1
-// if theres an outfile we dup2 this to stdout and close all pipe ends (if theres a list->next; meaning we piped)
-// if there is no infile and outfile we will setup the prev_pip and pip[WRITE] to stdin and stdout and closing other ends accordingly
-// if we are at te last node we have to close everything:
-//	.if prev_pipe != -1 close, if (list->prev close pip[READ] && pip[WRITE])
-
-// void	execute_command(t_list *list, int *pipe_input, int *pip, int pid, char **envp)
-// {
-// 	// we pipe if there is more than one list
-// 	if (list->next && pipe(pip) == -1)
-// 		error_message("pipe error", -1);
-// 	pid = fork();
-// 	if (pid == -1)
-// 		error_message("fork error", -1);
-// 	if (pid == 0)
-// 		child_process(list, pip, pipe_input, envp);
-// 	// if we piped
-// 	if (list->prev)
-// 	{
-// 		*pipe_input = pip[READ];
-// 		close(pip[READ]);
-// 		close(pip[WRITE]);
-// 	}
-// 	// if we didnt pipe we simply do nothing, no pipe to close, no prev_pipe
-// 	// we already closed in child when calling dup2 but must be also in parent
-// 	if (list->input >= 0)
-// 		close(list->input);
-// 	if (list->output >= 0)
-// 		close(list->output);
-// }
-
-// void	execute(t_list *list, char **envp, int pid_count)
-// {
-// 	pid_t	pid[pid_count];
-// 	int		pip[2];
-// 	int		pipe_input;
-// 	int		i;
-
-// 	pipe_input = -1;
-// 	i = 0;
-// 	while (i < pid_count)
-// 	{
-// 		// check if the node is valid and we can continue executing
-// 		if (list->input != -1 && list->output != -1)
-// 			execute_command(list, &pipe_input, pip, pid[i], envp);
-// 		list = list->next;
-// 		i++;
-// 	}
-// }
-
-
-// void    execute(t_list *list, char **envp)
-// {
-//     int     pid_count = count_pids(list);
-//     pid_t   pid[pid_count];
-//     int     pip[2];
-//     int     prev_pipe;
-//     int     i;
-
-//     prev_pipe = -1;
-//     i = 0;
-//     while (i < pid_count)
-//     {
-//         if (list->next && pipe(pip) == -1)
-//                 error_message("pipe_error", -1);
-//         pid[i] = fork();
-//         if (pid[i] == -1)
-//             error_message("fork error", -1);
-// 		printf("forking pid[%d]\n", i);
-//         if (pid[i] == 0)
-//             child_process(list, pip, prev_pipe, envp);
-// 		// sets prev pipes stdin for next command, if there is one
-// 		if (list->next)
-// 		{
-// 			prev_pipe = pip[READ];
-// 			close(pip[READ]);
-// 			close(pip[WRITE]);
-// 		}
-// 		// if theres no list->next theres no pipe
-// 		if (!list->next)
-// 			close(prev_pipe);
-// 		i++;
-//     }
-// }
-
 
 void	execute(t_list *list, char **envp, int pid_count)
 {
@@ -244,7 +129,7 @@ int main(int argc, char *argv[], char *envp[])
     (void)argc;
     (void)argv;
     t_list *list = NULL;
-    char *s = " cat err.log < info.txt | cat > outfile2 | echo info.txt < hello | cat > outfile3";
+    char *s = "ls -la > outfile| cat err.log";
     list = get_list(list, s, envp);
     if (!list)
     {
