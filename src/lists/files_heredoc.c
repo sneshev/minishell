6608ebf3020482fb4ac69180c52ff63a688b640d
sneshev <steffal.n2@gmail.com>
2025-07-17 19:26:12 +0200

    heredoc signals almost working | super weird vs code malfunction

--- src/lists/files_heredoc.c
+++ src/lists/files_heredoc.c
@@ -3,36 +3,73 @@
 #include "list.h"
 #include <readline/readline.h>
 #include <readline/history.h>
+#include <sys/wait.h>
 #include <fcntl.h>
+
+
+void heredoc_delim_msg(int line, char *delim)
+{
+	printf("warning: here-document at line ");
+	printf("%d ", line);
+	printf("delimited by end-of-file ");
+	printf("(wanted `%s')\n", delim);
+}
+
+void child_heredoc(char *delim, int pipefd[2])
+{
+	char	*line;
+
+	// enable_signals();
+	close(pipefd[0]);
+
+	while (1)
+	{
+		line = readline("> ");
+		if (!line)
+		{
+			heredoc_delim_msg(-42, delim);
+			free(line);
+			break ;
+		}
+		if (ft_strcmp(line, delim) == 0)
+		{
+			free(line);
+			break;
+		}
+		write(pipefd[1], line, ft_strlen(line));
+		write(pipefd[1], "\n", 1);
+		free(line);
+	}
+	close(pipefd[1]);
+	exit(0);
+}
+
 
 int	handle_heredoc(t_file *file)
 {
-	int pipefd[2];
-	char *line;
-	char *delim;
+	int		pipefd[2];
+	int		status;
+	pid_t	pid;
 
-	delim = file->filename;
 	if (pipe(pipefd) == -1)
-	{
-		// error
 		return (-1);
-	}
-	line = readline("> ");
-	while (line)
+
+	pid = fork();
+	if (pid == -1)
+		return (-1);
+
+	if (pid == 0)
+		child_heredoc(file->filename, pipefd);
+	else
 	{
-		if (strcmp(line, delim) == 0)
+		disable_SIGINT();
+		close(pipefd[1]);
+		waitpid(pid, &status, 0);
+		if (WIFSIGNALED(status))
 		{
-			free(line);
-			line = NULL;
-		}
-		else
-		{
-			write(pipefd[1], line, ft_strlen(line));
-			write(pipefd[1], "\n", 1);
-			free(line);
-			line = readline("> ");
+			close(pipefd[0]);
+			return (-1);
 		}
 	}
-	close (pipefd[1]);
 	return (pipefd[0]);
 }
