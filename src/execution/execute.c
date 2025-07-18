23b777d9ef16c29ee67d9f0abf549edd9dc559a5
John Doe <steffal.n2@gmail.com>
2025-07-18 13:03:13 +0200

    SIGNALS FULLY DONE (i think) LESGOOOOOOO

--- src/execution/execute.c
+++ src/execution/execute.c
@@ -133,7 +133,14 @@ int	wait_for_pids(pid_t *pid, int pid_count)
 	waitpid(pid[i], &status, 0);
 	if (WEXITSTATUS(status))
 		exitcode = (WEXITSTATUS(status));
-	// reset_signals();
+	if (WIFSIGNALED(status))
+	{
+		if (WTERMSIG(status) == SIGQUIT)
+			write(STDOUT_FILENO, "Quit (core dumped)\n", 20);
+		else if (WTERMSIG(status) == SIGINT)
+			write(STDOUT_FILENO, "\n", 1);
+	}
+
 	return (exitcode);
 }
 
