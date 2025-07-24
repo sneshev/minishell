f9167ace6a59adbc84f984489fe3ae4c9ff9081c
Mitani Misumi <mmisumi@f1r5s15.codam.nl>
2025-07-24 17:21:19 +0200

    forgot some changes

--- src/execution/builtins/execute_builtin.c
+++ src/execution/builtins/execute_builtin.c
@@ -6,7 +6,7 @@
 /*   By: mmisumi <mmisumi@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/05 15:35:28 by mmisumi           #+#    #+#             */
-/*   Updated: 2025/07/24 17:16:00 by mmisumi          ###   ########.fr       */
+/*   Updated: 2025/07/24 17:18:32 by mmisumi          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -70,7 +70,6 @@ int	execute_cd(t_list *list, t_env *env)
 	if (i > 2)
 		return (write_err("cd", "too many arguments"), 1);
 	new_dir = list->args[1];
-	//not the good error message
 	if (chdir(new_dir) == -1)
 		return (cd_error(new_dir), 1);
 	return (0);
@@ -86,71 +85,4 @@ int	execute_pwd(void)
 		return (1);
 	printf("%s\n", dir);
 	return (0);
-}
-
-// int	execute_unset(t_list *list, t_env **env)
-// {
-
-// }
-
-
-bool	is_valid_code(char *str)
-{
-	int len = 0;
-	char sign;
-
-	while (is_space(*str))
-		str++;
-	sign = 'p';
-	if (*str == '-' || *str == '+')
-	{
-		if (*str == '-')
-			sign = 'n';
-		str++;
-	}
-	if (!*str)
-		return (false);
-	while(*str == '0')
-		str++;
-    if (!*str)
-        return (true);
-    while (ft_isdigit(str[len]))
-        len++;
-    if (str[len] != '\0')
-        return (false);
-    if (len > 19)
-        return (false);
-    if (len == 19)
-    {
-        if (sign == 'p' && ft_strcmp(str, "9223372036854775807") > 0)
-            return (false);
-        if (sign == 'n' && ft_strcmp(str, "9223372036854775808") > 0)
-            return (false);
-    }
-    return (true);
-}
-
-int	execute_exit(t_list *list)
-{
-	int	arg_count;
-
-	write(1, "exit\n", 5);
-	arg_count = count_strings(list->args, true) - 1;
-	// printf("arg_count: %d\n", count_strings(list->args, true));
-	if (arg_count == 0)
-		exit_with_code(0);
-	else if (arg_count == 1)
-	{
-		if (is_valid_code((list->args)[1]))
-			exit_with_code(ft_atol((list->args)[1]) % 256);
-		else
-		{
-			write(1, "exit: ", 6);
-			write_err((list->args)[1], "numeric argument required");
-			exit_with_code(2);
-		}
-	}
-	else
-		write_err("exit", "too many arguments");
-	return (1);
 }
---
+++ src/execution/builtins/exit.c
@@ -0,0 +1,76 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   exit.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mmisumi <mmisumi@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/07/24 17:18:14 by mmisumi           #+#    #+#             */
+/*   Updated: 2025/07/24 17:18:36 by mmisumi          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../../minishell.h"
+#include "../execution.h"
+
+
+bool	is_valid_code(char *str)
+{
+	int len = 0;
+	char sign;
+
+	while (is_space(*str))
+		str++;
+	sign = 'p';
+	if (*str == '-' || *str == '+')
+	{
+		if (*str == '-')
+			sign = 'n';
+		str++;
+	}
+	if (!*str)
+		return (false);
+	while(*str == '0')
+		str++;
+    if (!*str)
+        return (true);
+    while (ft_isdigit(str[len]))
+        len++;
+    if (str[len] != '\0')
+        return (false);
+    if (len > 19)
+        return (false);
+    if (len == 19)
+    {
+        if (sign == 'p' && ft_strcmp(str, "9223372036854775807") > 0)
+            return (false);
+        if (sign == 'n' && ft_strcmp(str, "9223372036854775808") > 0)
+            return (false);
+    }
+    return (true);
+}
+
+int	execute_exit(t_list *list)
+{
+	int	arg_count;
+
+	write(1, "exit\n", 5);
+	arg_count = count_strings(list->args, true) - 1;
+	// printf("arg_count: %d\n", count_strings(list->args, true));
+	if (arg_count == 0)
+		exit_with_code(0);
+	else if (arg_count == 1)
+	{
+		if (is_valid_code((list->args)[1]))
+			exit_with_code(ft_atol((list->args)[1]) % 256);
+		else
+		{
+			write(1, "exit: ", 6);
+			write_err((list->args)[1], "numeric argument required");
+			exit_with_code(2);
+		}
+	}
+	else
+		write_err("exit", "too many arguments");
+	return (1);
+}
