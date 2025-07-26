8888058214b32267f3c8a6005546802fe9d6957b
Mitani Misumi <mmisumi@f0r1s12.codam.nl>
2025-07-26 14:12:12 +0200

    saddes isatty

--- src/main.c
+++ src/main.c
@@ -6,18 +6,20 @@
 /*   By: mmisumi <mmisumi@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/27 16:08:45 by mmisumi           #+#    #+#             */
-/*   Updated: 2025/07/24 17:27:04 by mmisumi          ###   ########.fr       */
+/*   Updated: 2025/07/26 14:10:30 by mmisumi          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 #include "signals/signals.h"
+#include "history/history.h"
 #include <readline/readline.h>
 #include <readline/history.h>
 
 // void	minishell(char *envp[])
 void	minishell(char **envp)
 {
+	char	*line;
 	t_list	*list;
 	t_env	*env;
 	int		exitcode;
@@ -31,14 +33,16 @@ void	minishell(char **envp)
 	while (1)
 	{
 		enable_signals();
-		char	*line;
-		line = readline("minishell$ ");
-		// line = "^C";
-
+		if (isatty(STDIN_FILENO) == true)
+		{
+			line = readline("minishell$ ");
+			ft_add_history(line);
+		}
+		else
+			line = get_next_line(STDIN_FILENO);
 		if (!line)
 			exit_with_code(0);
 		
-		ft_add_history(line);
 		list = get_list(list, line, env);
 		if (!list)
 		{
@@ -66,6 +70,7 @@ int main(int argc, char *argv[], char *envp[])
 	// 	"goodmorning=sunsunsun\n",
 	// 	"binkie=cutecutecute\n",
 	// 	NULL};
+
 	minishell(envp);
 	return (0);
 }
