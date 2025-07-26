eb2cd988fa1dfe52f889333ac23ccc6017f7e9ab
John Doe <steffal.n2@gmail.com>
2025-07-26 15:41:42 +0200

    normde minishell.h and exit_code_addr.c

--- src/utils/exit_code_addr.c
+++ src/utils/exit_code_addr.c
@@ -1,21 +1,33 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   exit_code_addr.c                                   :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: sneshev <sneshev@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/07/26 15:39:49 by sneshev           #+#    #+#             */
+/*   Updated: 2025/07/26 15:41:06 by sneshev          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "../minishell.h"
 
-static int *exit_code_addr()
+static int	*exit_code_addr(void)
 {
-    static int exit_code = 0;
+	static int	exit_code = 0;
 
-    return (&exit_code);
+	return (&exit_code);
 }
 
-void set_exit_code(int new_code)
+void	set_exit_code(int new_code)
 {
-    int *exit_code_ptr;
+	int	*exit_code_ptr;
 
-    exit_code_ptr = exit_code_addr();
-    *exit_code_ptr = new_code;
+	exit_code_ptr = exit_code_addr();
+	*exit_code_ptr = new_code;
 }
 
-int get_exit_code(void)
+int	get_exit_code(void)
 {
-    return (*(exit_code_addr()));
-}   
+	return (*(exit_code_addr()));
+}
