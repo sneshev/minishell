8f1982a62f596d3bfd11cd623c7c4dbb040d6852
sneshev <steffal.n2@gmail.com>
2025-09-06 16:27:50 +0200

    can run with arg

--- src/main.c
+++ src/main.c
@@ -6,7 +6,7 @@
 /*   By: stefuntu <stefuntu@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/27 16:08:45 by mmisumi           #+#    #+#             */
-/*   Updated: 2025/08/08 11:36:55 by stefuntu         ###   ########.fr       */
+/*   Updated: 2025/09/06 16:26:27 by stefuntu         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,23 +15,56 @@
 #include "history/history.h"
 #include <readline/readline.h>
 #include <readline/history.h>
+#include <fcntl.h>
 
-char	*ft_readline(void)
+#define UNINITIALIZED -2
+
+char	*take_arg_nextline(char *arg)
+{
+	char		*line;
+	static int	fd = UNINITIALIZED;
+
+	if (fd == UNINITIALIZED)
+	{
+		if (access(arg, F_OK) != 0)
+			return (write_err(arg, "No such file or directory"), NULL);
+		else if (access(arg, R_OK) != 0)
+			return (write_err(arg, "Permission denied"), NULL);
+		fd = open(arg, O_RDONLY, 0400);
+		if (fd == -1)
+			return (NULL);
+	}
+	line = get_next_line(fd);
+	if (!line)
+	{
+		close(fd);
+		exit(get_exit_code());
+	}
+	return (line);
+}
+
+char	*ft_readline(bool has_arg, char *arg)
 {
 	char	*line;
 
-	if (isatty(STDIN_FILENO) == true)
+	if (has_arg)
+		line = take_arg_nextline(arg);
+	else if (isatty(STDIN_FILENO) == true)
 	{
 		line = readline("minishell$ ");
 		ft_add_history(line, false);
 	}
 	else
+	{
 		line = get_next_line(STDIN_FILENO);
+		if (!line)
+			exit(get_exit_code());
+	}
 	return (line);
 }
 
 // void	minishell(char *envp[])
-void	minishell(char **envp)
+void	minishell(char **envp, bool has_arg, char *arg)
 {
 	char	*line;
 	t_list	*list;
@@ -43,7 +76,7 @@ void	minishell(char **envp)
 	while (1)
 	{
 		enable_signals();
-		line = ft_readline();
+		line = ft_readline(has_arg, arg);
 		if (!line)
 		{
 			write(1, "exit\n", 5);
@@ -64,6 +97,8 @@ int	main(int argc, char *argv[], char *envp[])
 {
 	(void)argv;
 	if (argc == 1)
-		minishell(envp);
+		minishell(envp, false, NULL);
+	else
+		minishell(envp, true, argv[1]);
 	return (0);
 }
